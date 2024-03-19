0ee4e0fde686b8462e463d1e702e3d0555fe92b3
AnudeepVvs <134407684+AnudeepVvs@users.noreply.github.com>
2024-03-19 11:24:16 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { CrewComponent } from "./crew/crew.component";
+import {TechnologyComponent} from"./technology/technology.component";
+import { ExperimentsComponent } from "./experiments/experiments.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, CrewComponent, TechnologyComponent, ExperimentsComponent]
+})
+export class AppComponent {
+  title = 'angular_mine';
+}
